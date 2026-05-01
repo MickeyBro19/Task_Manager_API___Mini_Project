0038bb0d85fc2fb4fdb968fc731c5904b67f1dcc
Mickey <bhaskarp986@gmail.com>
2026-05-01 19:27:26 +0530

    Initial Commit: Created a new project with name "Task Manager Api" components added: Spring web, security, jpa, Postgres Sql Driver, Lombok

---
+++ src/main/java/com/mickey/task_manager_api/TaskManagerApiMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mickey.task_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApiMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApiMiniProjectApplication.class, args);
+    }
+
+}
